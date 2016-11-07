9190d127398eacbe9565c112009fff8e8aa43d4b
michaelplott <mike.plott80@gmail.com>
2016-11-07 10:36:37 -0500

    Initial push

---
+++ src/Room.java
@@ -0,0 +1,15 @@
+/**
+ * Created by michaelplott on 11/7/16.
+ */
+public class Room {
+    int row;
+    int col;
+    boolean wasVisited = false;
+    boolean hasRight = true;
+    boolean hasBottom = true;
+
+    public Room(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
